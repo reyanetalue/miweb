abc14c32ec76dbafeee4289ad461a9c514213a4b
Nayer Eulate <nayereulate@gmail.com>
2024-11-11 19:58:57 -0400

    Agregue el nombre del repositorio

--- prueba.java
+++ prueba.java
@@ -1,5 +1,6 @@
 public class prueba {
     public static void main(String arg[]){
         System.out.println("Sale fort xd");
+        System.out.println("https://github.com/reyanetalue/miweb.git");
     }
 }
